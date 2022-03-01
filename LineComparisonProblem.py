eb97a0b2c77563ceb668ba2437669848bae684ce
unknown <aavik743@gmail.com>
2022-03-01 23:21:50 +0530

    added: co-ordinates of line

--- LineComparisonProblem.py
+++ LineComparisonProblem.py
@@ -1,16 +1,16 @@
 import math
 
 
-def line_length():
+def line_length():  # function to take input and compute the length of the line
     x1 = 2
     y1 = 3
     x2 = 4
     y2 = 5
 
-    res = math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))
+    res = math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))  # computation
     print("\nThe required length is", res)
 
 
 # Driver Code
 if __name__ == '__main__':
-    line_length()
+    line_length()  # function calling
